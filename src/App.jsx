f58419d87c7f3c3403fab2610b6e07f7dc5e8481
ggonep <guillermo.rgo92@gmail.com>
2023-02-28 16:30:37 -0300

    First commit ggbook con React y Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { NavBar } from './components/NavBar';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-indigo-500'>hola</h1>
+     
+      <div>
+        <NavBar/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <div className='bg-indigo-300 text-red-200'>NavBar</div>
+  )
+}
