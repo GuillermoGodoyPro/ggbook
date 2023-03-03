dd9ef52f19b55f1f1d032920246736fe90b0b025
ggonep <guillermo.rgo92@gmail.com>
2023-03-02 23:08:49 -0300

    Banner ready aaaaaahhh

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,7 +5,7 @@ import Image from '../assets/backupimg/perfilcB.png'
 // Icons
 import { FaYoutube, FaDribbble, FaGithub } from 'react-icons/fa'
 // Type animation
-import { TypeAnimation } from 'react-type-animation'
+import Typewriter from 'typewriter-effect';
 // Motion
 import { motion } from 'framer-motion'
 // variants
@@ -15,39 +15,72 @@ import { fadeIn } from '../variants'
 
 export const Banner = () => {
   return (
-    <section className='section' id='banner'>
+    <section className='min-h-[85vh] lg:min-h-[78vh] flex items-center' id='home'>
       <div className='container mx-auto' >
-        <div className='flex flex-col gap-y-8 lg:flex-row'>  
+        <div className='flex flex-col gap-y-8 lg:flex-row lg:gap-x-12'>  
           {/*---- all div of text with animation ----*/}               
           <div className='flex-1 text-center font-secondary lg:text-left'>
-            <h1 className='text-[55px] font-bold leading-[0.8] lg:text[110px]'>
-              GODOY <span >GUILLERMO</span>
-            </h1>
-            <div className='mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold uppercase leading-[1]'>
+            <motion.h1 
+              variants={fadeIn('up', 0.3)} 
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='text-[55px] font-bold leading-[0.8] lg:text[110px]
+            '>
+                GODOY <span >GUILLERMO</span>
+            </motion.h1>
+            <motion.div 
+              variants={fadeIn('up', 0.5)} 
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-6 text-[36px] lg:text-[55px] font-secondary font-semibold uppercase leading-[1]'
+            >
               <span className='text-white mr-4'>I am a </span>
-              <TypeAnimation sequence={[
-                'MERN Developer',
-                2000,
-                'Web Designer',
-                2000,
-                'System Analyst',
-                2000,
-              ]}
-                speed={50}
-                className='text-accent'
-                wrapper='span'
-                repeat={Infinity}
-              />
-            </div>
-            <p className='mb-8 max-w-lg mx-auto lg:mx-0'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum in accusantium atque omnis tempore fuga suscipit error qui iusto sit.</p>
-            <div className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0'>
+              <Typewriter
+                
+                wrapper='span'                
+                options={{
+                  strings: ['MERN Developer','Web Designer','System Analyst'],
+                  autoStart: true,
+                  delay:100,
+                  deleteSpeed: 50,
+                  loop: true,    
+                  wrapperClassName: 'text-accent',             
+                }}
+               
+              />                
+             
+            </motion.div>
+            <motion.p 
+              variants={fadeIn('up', 0.5)} 
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='mb-8 max-w-lg mx-auto lg:mx-0'
+            >
+               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Amet libero ipsum, quo dignissimos dolorem molestiae reprehenderit suscipit reiciendis beatae enim!
+            </motion.p>
+            <motion.div
+              variants={fadeIn('up', 1.2)} 
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0' 
+            >
               <button className='btn btn-lg'>Contact me</button>
               <a href="https://github.com/GuillermoGodoyPro" className='text-gradient btn-link'>
                 My GGBook
               </a>
-            </div>  
+            </motion.div>  
             {/* socials */}
-            <div className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'>
+            <motion.div 
+              variants={fadeIn('up', 1.2)} 
+              initial="hidden"
+              whileInView={'show'}
+              viewport={{once: false, amount: 0.7}}
+              className='flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0'
+            >
               <a href='https://github.com/GuillermoGodoyPro'>
                 <FaYoutube />
               </a>
@@ -57,15 +90,20 @@ export const Banner = () => {
               <a href='https://github.com/GuillermoGodoyPro'>
                 <FaDribbble />
               </a>
-            </div>
+            </motion.div>
                       
           </div>
-          <div className='mx-auto '> {/* ------------- image ----------- */} {/* 2xl:flex xl:flex lg:flex  md:hidden sm:hidden xs:hidden */}
+          <motion.div 
+            variants={fadeIn('down', 0.8)} 
+            initial="hidden"
+            whileInView={'show'}
+            className='hidden lg:flex xl:flex 2xl:flex max-w-[21.5em] lg:max-w-[26em]'
+          > {/* ------------- image ----------- */} {/* 2xl:flex xl:flex lg:flex  md:hidden sm:hidden xs:hidden */}
             <div>
               <img src={Image} alt="ggCBloob" />
               
             </div>
-          </div> 
+          </motion.div> 
         </div>  
       </div>
     </section>
