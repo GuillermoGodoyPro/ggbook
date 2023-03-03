6b01f954b6d0a7f11c949282e8cd66b63146e6b9
ggonep <guillermo.rgo92@gmail.com>
2023-03-03 14:56:05 -0300

    Start Services

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
+/* icon */
+import { BsArrowUpRight } from 'react-icons/bs'
+// motion
+import {motion} from 'framer-motion'
+// variant 
+import { fadeIn } from '../variants'
+
+
+// services data
+const services = [
+  {
+    name: 'Graphic design',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sit.',
+    link: 'Learn More'
+  },
+  {
+    name: 'Development',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sit.',
+    link: 'Learn More'
+  },
+  {
+    name: 'Business Services',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, sit.',
+    link: 'Learn More'
+  },
+]
 
 export const Services = () => {
   return (
-    <section className='section' id='services'>Services</section>
+    <section className='section' id='services'>
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <h2 className='h2'>What I Do.</h2>
+          {/* services */}
+          <div>services</div>
+          
+        </div>
+      </div>
+    </section>
   )
 }
