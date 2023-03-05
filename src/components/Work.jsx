7ef5d14afe26ffa66b953fd53bb2e6af445282ee
ggonep <guillermo.rgo92@gmail.com>
2023-03-05 10:06:27 -0300

    Updated sise work

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -16,11 +16,11 @@ export const Work = () => {
       <div className='container mx-auto'>
         <div className='flex flex-col lg:flex-row gap-x-10'>
           <motion.div 
-            variants={fadeIn('up', 0.2)}
+            variants={fadeIn('right', 0.2)}
             initial='hidden'
             whileInView={'show'}
             viewport={{ once: false, amount: 0.3}}  
-            className='flex-1 flex flex-col gap-y-16 mb-10 lg:mb-0'
+            className='flex-1 flex flex-col gap-y-10 mb-10 lg:mb-0'
           >
             {/* text */}
             <div>
@@ -28,7 +28,7 @@ export const Work = () => {
                 My Latest <br/>
                 Work.
               </h2>
-              <p className='max-w-sm mb-16'>
+              <p className='max-w-sm mb-10'>
                 Lorem, ipsum dolor sit amet consectetur adipisicing elit. A recusandae, nulla saepe explicabo cupiditate rerum consectetur accusamus ipsum sint molestiae?
               </p>
               <button className='btn btn-sm'>View all projects</button>
